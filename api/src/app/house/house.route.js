85de65abd3d1279e46d1d5e50e20d5633f38f00f
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-12-06 20:11:44 +0600

    housed updation function added

--- api/src/app/house/house.route.js
+++ api/src/app/house/house.route.js
@@ -1,10 +1,11 @@
-const { createHouse, deleteHouse, getSingleHouse, getAllHouse } = require('./house.controller');
+const { createHouse, deleteHouse, getSingleHouse, getAllHouse, updateHouse } = require('./house.controller');
 const Router= require('express').Router();
 
 
 Router.post('/set/house-rent', createHouse)
 Router.get('/house', getAllHouse)
 Router.get('/house/:houseId', getSingleHouse)
+Router.patch('/house/:houseId', updateHouse)
 Router.delete('/house', deleteHouse)
 
 module.exports = Router;
